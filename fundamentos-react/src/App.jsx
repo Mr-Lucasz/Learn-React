7b8dfb33381d7a4ef97abdcca6ef0ce663e885c4
Lucas Rodrigues <l.cunha14.lc@gmail.com>
2023-08-07 00:03:23 -0300

    Mock Post

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,8 +1,66 @@
 import { Post } from "./components/Post";
 import { Header } from "./components/Header";
-import { Sidebar } from './components/Sidebar';
-import styles from './App.module.css';
-import './Global.css';
+import { Sidebar } from "./components/Sidebar";
+import styles from "./App.module.css";
+import "./Global.css";
+
+// author: {avatar_url: "", name: "", role: ""}
+//publishedAt: date
+//content : string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/Mr-Lucasz.png",
+      name: "Lucas Rodrigues",
+      role: "QA Analyst Sr"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋", },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",},
+      { type: "link", content: "👉 jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-08-06 23:31:56"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/Mr-Lucasz.png",
+      name: "Lucas Rodrigues",
+      role: "PO"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "👉 jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-08-06 23:45:56"),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/Mr-Lucasz.png",
+      name: "Lucas Cunha",
+      role: "Engineer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "👉 jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2023-08-06 23:45:56"),
+  }
+];
+//iteração - estrutura de repetição
 
 export function App() {
   return (
@@ -11,9 +69,17 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <div className={styles.postContainer}>
-          <Post author="Lucas Rodrigues" />
-          <Post author="Lucas Cunha" />
-          <Post author="Lucas Rodrigues" />
+          {posts.map((post) => {
+            return (
+              <Post
+
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+              
+            );
+          })}
         </div>
       </div>
     </div>
--- fundamentos-react/src/components/Post.jsx
+++ fundamentos-react/src/components/Post.jsx
@@ -1,19 +1,21 @@
-
 import { Avatar } from "./Avatar";
-import  {Comment}  from "./Comment";
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
+// author: {avatar_url: "", name: "", role: ""}
+//publishedAt: date
+//content : string
+
 export function Post(props) {
+  console.log(props);
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar  src="https://github.com/Mr-Lucasz.png"
-            alt="Avatar"
-          />
+          <Avatar src={props.author.avatarUrl} alt="Avatar" />
           <div className={styles.authorInfo}>
-            <strong>{props.author}</strong>
-            <span>QA Analyst</span>
+            <strong>{props.author.name}</strong>
+            <span>{props.author.role}</span>
           </div>
         </div>
         <time title="21 de Junho às 22:09h" dateTime="2023-06-11 22:09:56">
@@ -21,21 +23,19 @@ export function Post(props) {
         </time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          👉<a href="#"> jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="https://github.com/Mr-Lucasz/Learn-React">#novoprojeto </a>
-          {""}
-          <a href="https://www.rocketseat.com.br/nlw">#nlw </a>
-          {""}
-          <a href="https://www.rocketseat.com.br/">#rocketseat </a>
-        </p>
+        {props.content.map((item, index) => {
+          if (item.type === "paragraph") {
+            return <p key={index}>{item.content}</p>;
+          } else if (item.type === "link") {
+            return (
+              <p key={index}>
+                <a href={item.content}>{item.content}</a>
+              </p>
+            );
+          } else {
+            return null; // Handle other content types if needed
+          }
+        })}
       </div>
       <form className={styles.commentForm}>
         <strong>Deixa seu feedback</strong>
@@ -45,8 +45,8 @@ export function Post(props) {
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
+        <Comment />
+        <Comment />
       </div>
     </article>
   );
